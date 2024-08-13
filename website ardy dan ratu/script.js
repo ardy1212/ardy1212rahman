0d1b2ee010bfaff647ae44fa12d170d9a5e37e92
ardy1212 <ardyr1139@gmail.com>
2024-08-13 12:48:44 +0700

    Add files via upload

---
+++ website ardy dan ratu/script.js
@@ -0,0 +1,4 @@
+// Script ini opsional, bisa digunakan untuk menambah interaktivitas
+document.addEventListener('DOMContentLoaded', function() {
+    // Tambahkan interaksi JavaScript di sini jika diperlukan
+});
